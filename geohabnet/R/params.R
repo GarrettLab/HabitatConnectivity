660da0a1c6e407e8bb1178f820d40ca8d05faaaf
Krishna Keshav <kkeshav@ufl.edu>
2023-05-17 18:49:18 +0530

    whatever's remaining

--- geohabnet/R/params.R
+++ geohabnet/R/params.R
@@ -22,13 +22,3 @@ get_parameters <- function(iwindow = FALSE, out_path = getwd()) {
   print(paste("parameters are now available at - ", out_path, "/parameters.yaml"))
   return(file.path(paste(out_path, "parameters.yaml", sep = "/")))
 }
-
-set_parameter <- function(name, value = NULL) {
-  if(is.null(name) || !is.character(name))
-    stop("Invalid parameter name:", name)
-  params_file_path <- .get_param_file_path()
-  # Read the YAML file
-  yaml_data <- yaml.load_file(params_file_path)
-  yaml_data$name <- value
-  yaml.dump(yaml_data, file = params_file_path)
-}
