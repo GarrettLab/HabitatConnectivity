00361737d168da6bec471f5175ec7cc1fe431bae
Krishna Keshav <kkeshav@ufl.edu>
2023-05-25 20:06:38 +0530

    fixed set-param fun

--- geohabnet/R/ccri_helper.R
+++ geohabnet/R/ccri_helper.R
@@ -70,4 +70,42 @@ calculate_metrics_weight <- function(betweenness_metric = FALSE, node_strength =
   return(file_path)
 }
 
+.check_yaml_structure <- function(existing_yaml_file, provided_yaml_file) {
+  # Read the existing YAML file
+  existing_yaml <- yaml::yaml.load_file(existing_yaml_file)
+  
+  # Read the provided YAML file
+  provided_yaml <- yaml::yaml.load_file(provided_yaml_file)
+  
+  # Compare the structure of the YAML files
+  if (!identical(names(existing_yaml), names(provided_yaml))) {
+    stop("The provided YAML file does not match the structure of the existing YAML file.")
+  }
+  
+  # Recursively check the structure of nested sections
+  check_nested_structure <- function(existing_section, provided_section, section_name) {
+    if (!identical(names(existing_section), names(provided_section))) {
+      stop(paste("The", section_name, "section in the provided YAML file does not match the structure of the existing YAML file."))
+    }
+    
+    # Check the structure of nested sections
+    for (key in names(existing_section)) {
+      if (is.list(existing_section[[key]]) && is.list(provided_section[[key]])) {
+        check_nested_structure(existing_section[[key]], provided_section[[key]], paste(section_name, key, sep = " > "))
+      }
+    }
+  }
+  
+  # Check the structure of nested sections
+  for (key in names(existing_yaml)) {
+    if (is.list(existing_yaml[[key]]) && is.list(provided_yaml[[key]])) {
+      check_nested_structure(existing_yaml[[key]], provided_yaml[[key]], key)
+    }
+  }
+  
+  # If the function reaches this point, the YAML structures match
+  return(TRUE)
+}
+
+
 
