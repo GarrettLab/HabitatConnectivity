7f67add716ca5a6651d5e892b14d8e9fc43b6700
Krishna Keshav <kkeshav@ufl.edu>
2023-06-19 20:33:08 +0530

    fixed installation issue

--- geohabnet.Rcheck/tests/testthat/test-functional.R
+++
@@ -1,9 +0,0 @@
-library("testthat")
-
-test_that("System test - Test function to retreive crop raster data", {
-  expect_no_condition(geohabnet::get_cropharvest_raster("avocado"))
-})
-
-test_that("System test - Test Senstivity analysis run on default configuration", {
-  expect_no_condition(geohabnet::senstivity_analysis(), message = "Senstivity analysis completed")
-})
