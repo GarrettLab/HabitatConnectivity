71ad6840f32cc1668a2523361feb8eefb774ed69
Krishna Keshav <kkeshav@ufl.edu>
2024-06-17 08:39:20 -0400

    nd impl -wip

--- geohabnet/R/network_density.R
+++ geohabnet/R/network_density.R
@@ -11,7 +11,7 @@ network_density <- function(x) {
   
   for (adm in x@rasters$rasters) {
     ed <- igraph::edge_density(igraph::graph_from_adjacency_matrix(adm@amatrix > 0))
-    if (adm@beta != -1) {
+    if (!is.nan(adm@beta)) {
       betas <- append(betas, adm@beta)
       eds_beta <- append(eds_beta, ed)
     } else {
@@ -22,17 +22,17 @@ network_density <- function(x) {
 
   plotted <- FALSE
   plotted <- if (length(betas) > 0) {
-    plot(betas, eds_beta, xlab = "Beta", ylab = "Network density", type = "p", col = "red", cex = 3)
-    lines(betas, net.density.mean, col = "blue", lwd = 2)
+    plot(betas, eds_beta, xlab = "Beta", ylab = "Network density", type = "p", col = "red", bg = "red", cex = 1)
+    lines(betas, eds_beta, col = "blue", lwd = 2)
     TRUE
   }
 
   if (plotted & length(gammas) > 0) {
-    points(gammas, eds_gamma, col = "green", cex = 3)
-    lines(gammas, net.density.mean, col = "blue", lwd = 2)
+    points(gammas, eds_gamma, xlab = "Gamma", ylab = "Network density", type = "p", col = "red", cex = 1)
+    lines(gammas, eds_gamma, col = "blue", lwd = 2)
   }
   else if (length(gammas) > 0) {
-    plot(gammas, eds_gamma, xlab = "Gamma", ylab = "Network density", type = "p", col = "green", cex = 3)
-    lines(gammas, net.density.mean, col = "blue", lwd = 2)
+    plot(gammas, eds_gamma, xlab = "Gamma", ylab = "Network density", type = "p", col = "green", cex = 1)
+    lines(gammas, eds_gamma, col = "blue", lwd = 2)
   }
 }
